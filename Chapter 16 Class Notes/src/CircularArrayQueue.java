d616112bd2189b51fe30e4477147875ac35b82cd
Ethan He <42473043+ethanlhe@users.noreply.github.com>
2019-09-18 14:21:37 -0500

    bruh

--- Chapter 16 Class Notes/src/CircularArrayQueue.java
+++ Chapter 16 Class Notes/src/CircularArrayQueue.java
@@ -1,8 +1,11 @@
+/*
 import java.util.NoSuchElementException;
 
+*/
 /**
     An implementation of a queue as a circular array.
-*/
+*//*
+
 public class CircularArrayQueue
 {
     private Object[] elements;
@@ -10,44 +13,54 @@ public class CircularArrayQueue
 
 
 
-    /**
-        Constructs an empty queue.
     */
+/**
+        Constructs an empty queue.
+    *//*
 
 
 
 
 
 
-    /**
+
+    */
+/**
         Checks whether this queue is empty.
         @return true if this queue is empty
+    *//*
+
+
+
+
+
     */
-
-
-
-
-    /**
+/**
         Adds an element to the tail of this queue.
         @param newElement the element to add
+    *//*
+
+
+
+
+
+
     */
-
-
-
-
-
-    /**
+/**
         Removes an element from the head of this queue.
         @return the removed element
+    *//*
+
+
+
+
+
+
     */
-
-
-
-
-
-    /**
+/**
         Grows the element array if the current size equals the capacity.
-    */
+    *//*
+
     private void growIfNecessary()
     {
         if(this.currentSize == this.elements.length)
@@ -67,3 +80,4 @@ public class CircularArrayQueue
 
 
 }//CircularArrayQueue
+*/
